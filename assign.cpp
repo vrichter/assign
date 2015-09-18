c90f43df536da32bbb2828868ebace525dd8dbf6
Viktor Richter <vrichter@posteo.de>
2015-09-18 12:58:54 +0200

    Switch entity for participant with first and last name.

--- assign.cpp
+++ assign.cpp
@@ -38,12 +38,18 @@
 #define WARNING(TXT) { std::cout << "WARNING: " << TXT<< std::endl; }
 #define ERROR(ERRVAL,TXT) { std::cout << "ERROR: " << TXT << std::endl; exit(ERRVAL); }
 
+struct Participant {
+  std::string id = "";
+  std::string first_name = "";
+  std::string last_name = "";
+  std::vector<int> preferences;
+};
+
 typedef unsigned int uint;
 typedef int Cost;
-typedef std::vector<std::vector<Cost> > CostMatrix;
 typedef std::string Id;
-typedef std::vector<int> Preferences;
-typedef std::pair<Id,Preferences> Entity;
+typedef int GroupId;
+typedef std::map<std::string,std::string> ProgArgs;
 
 std::vector<std::string> split(const std::string& data, char delimiter) {
   std::vector<std::string> result;
@@ -72,67 +78,84 @@ std::vector<T> parse_vector(const std::vector<std::string>& data, uint start_at=
   return result;
 }
 
-Entity parseEntity(const std::string& csv){
+Participant parseParticipant(const std::string& csv){
   std::vector<std::string> vector = split(csv,',');
-  Entity result;
-  result.first = parse<std::string>(vector.front());
-  result.second = parse_vector<int>(vector,1);
+  Participant result;
+  if(vector.size() > 3){
+    result.first_name = parse<std::string>(vector[0]);
+    result.last_name = parse<std::string>(vector[1]);
+    result.id = parse<std::string>(vector[2]);
+    result.preferences = parse_vector<int>(vector,3);
+  }
   return result;
 }
 
-void process_args(int arg_num, char** args){
+void process_args(int arg_num, char** args, ProgArgs& prog_args_dst){
+  // set default values
   for(int i = 1; i < arg_num; ++i){
     std::string arg = args[i];
     if(arg == "--help" || arg == "-h"){
-      std::cout << "This application uses preferences to sort entities into groups. Input data is read from stdin.\n"\
+      std::cout << "This application uses preferences to sort participants into groups. Input data is read from stdin.\n"\
                 << "Usage: assign < preferences.csv\n\n"
                 << "Parameters:\n"
                 << "\t -h | --help \t\t print this message and leave.\n"
-                << "\t -e | --example \t print an example preferences document in the correct format and leave."
+                << "\t -e | --example \t\t print an example preferences document in the correct format and leave."
                 << std::endl;
       std::exit(0);
     } else if (arg == "--example" || arg == "-e"){
-      std::cout << "jack,1,5,6,2,3" << std::endl;
-      std::cout << "jill,6,2,4,6,2" << std::endl;
-      std::cout << "paul,3,3,5,2,1" << std::endl;
-      std::cout << "will,2,9,7,4,3" << std::endl;
-      std::cout << "jenn,5,6,3,7,1" << std::endl;
+      std::cout << "Jack,Bauer,jack,1,5,6,2,3" << std::endl;
+      std::cout << "Jill,Bill,jill,6,2,4,6,2" << std::endl;
+      std::cout << "Paul,Walker,paul,3,3,5,2,1" << std::endl;
+      std::cout << "Will,Smith,will,2,9,7,4,3" << std::endl;
+      std::cout << "Jenn,When,jenn,5,6,3,7,1" << std::endl;
       std::exit(0);
     }
   }
 }
 
-void parse_csv_data(std::vector<Id>& entity_ids, std::vector<Preferences>& entity_preferences){
-  uint group_count = 0;
+std::vector<Participant> parse_csv_data(){
+  std::vector<Participant> result;
+  uint preferences_count = 0;
   uint line = 1;
   while(std::cin) {
-    std::string entity_csv;
-    std::getline(std::cin, entity_csv);
+    std::string participant_csv;
+    std::getline(std::cin, participant_csv);
     if(std::cin.eof()) break;
-    Entity entity = parseEntity(entity_csv);
-    entity_ids.push_back(entity.first);
-    entity_preferences.push_back(entity.second);
+    result.push_back(parseParticipant(participant_csv));
     if(line == 1){
-      group_count = entity.second.size();
+      preferences_count = result.front().preferences.size();
     } else  {
-      if(entity.second.size() != group_count){
-        std::cerr << "Error: Group count in line #" << line << " is defferent from previous. "
-                  << entity.second.size() << " != " << group_count << "(previous)." << std::endl;
-        std::exit(-1);
+      if(result.back().preferences.size() != preferences_count){
+        ERROR(-1,"Preferences count in line #" << line << " is defferent from previous. "
+                  << result.back().preferences.size() << " != " << preferences_count << "(previous).");
       }
     }
     ++line;
   }
+  return result;
 }
 
-void print_data(const std::vector<Id>& entity_ids, const std::vector<Preferences>& entity_preferences, std::ostream& dst){
-  assert(entity_ids.size() == entity_preferences.size());
-  for(uint i = 0;  i < entity_ids.size(); ++i){
-    dst << entity_ids[i] << ":\t";
-    for (int j = 0; j < entity_preferences[i].size(); ++j){
-      dst << entity_preferences[i][j] << " ";
+template<typename T>
+std::string to_string(std::vector<T> vector){
+  if(vector.size() == 0){
+    return "[ ]";
+  } else {
+    std::stringstream stream;
+    stream << "[ " << vector.front();
+    for(uint i = 1; i < vector.size(); ++i){
+      stream << ", " << vector[i];
     }
-    dst << std::endl;
+    stream << " ]";
+    return stream.str();
+  }
+}
+
+void print_data(const std::vector<Participant>& participants, std::ostream& dst){
+  for(auto participant : participants){
+    dst << "Participant: " << participant.id << std::endl;
+    dst << "\tFirst name:  " << participant.first_name << std::endl;
+    dst << "\tLast name:   " << participant.first_name << std::endl;
+    dst << "\tPreferences: " << to_string(participant.preferences) << "\n";
   }
 }
 
@@ -148,12 +171,10 @@ void print_matrix(const Matrix<T>& matrix, std::ostream& stream){
   stream << std::endl;
 }
 
-Matrix<double> create_matrix(const std::vector<Id>& entity_ids, const std::vector<Preferences>& entity_preferences,
-                      uint group_count)
-{
-  uint group_size = entity_ids.size() / group_count;
-  if(entity_ids.size() % group_count) { ++group_size; } // rounding up
-  uint columns = entity_ids.size(); // true count of entities
+Matrix<double> create_matrix(std::vector<Participant>& participants, uint group_count){
+  uint group_size = participants.size() / group_count;
+  if(participants.size() % group_count) { ++group_size; } // rounding up
+  uint columns = participants.size(); // true count of entities
   uint rows = group_count * group_size;
 
   Matrix<double> result(rows,columns);
@@ -161,7 +182,57 @@ Matrix<double> create_matrix(const std::vector<Id>& entity_ids, const std::vecto
   for(uint row = 0; row < rows; ++row){
     for(uint column = 0; column < columns; ++column){
       uint looped_row = row % group_count;
-      result(row, column) = entity_preferences[column][looped_row]; // transposing
+      result(row, column) = participants[column].preferences[looped_row]; // transposing
+    }
+  }
+  return result;
+}
+
+void print_assignments_csv(const std::vector<Participant>& participants, const std::vector<GroupId>& groups){
+  for(uint i = 0; i < participants.size(); ++i){
+    std::cout << participants.at(i).first_name << ","
+              << participants.at(i).last_name << ","
+              << participants.at(i).id << "," << groups.at(i) << std::endl;
+  }
+}
+
+void print_assignments_json(const std::vector<Participant>& participants, const std::vector<GroupId>& groups){
+  std::cout << "{\n    \"assignment\": [\n";
+  for(uint i = 0; i < participants.size(); ++i){
+    std::cout << "        {\n"
+              << "            \"first_name\": \"" << participants.at(i).first_name << "\",\n"
+              << "            \"last_name\":  \"" << participants.at(i).last_name  << "\",\n"
+              << "            \"id\":         \"" << participants.at(i).id         << "\",\n"
+              << "            \"group\":      " << groups.at(i) << "\n        }";
+    if(i+1 < participants.size()){
+      std::cout << ",\n";
+    } else {
+      std::cout << "\n";
+    }
+  }
+  std::cout << "    ]\n}" << std::endl;
+}
+
+std::vector<GroupId> create_assignments(const std::vector<Participant>& participants, const uint group_count, const Matrix<double>& solution){
+  std::vector<GroupId> result;
+  result.reserve(participants.size());
+  for(uint i = 0; i < participants.size(); ++i){
+    uint column = i;
+    std::vector<int> groups;
+    for (uint row = 0; row < solution.rows(); ++row){
+      if(solution(row,column) == 0){
+        groups.push_back(row % group_count);
+      }
+    }
+    if(groups.size() == 0){
+      WARNING("Participant " << participants[i].id << " could not be assigned to a group");
+      result.push_back(-1);
+    } else {
+      if(groups.size() > 1){
+        WARNING("Participant " << participants[i].id << " was assigned to multiple groups: " << to_string(groups)
+                << " will use the first.");
+      }
+      result.push_back(groups.front());
     }
   }
   return result;
@@ -169,19 +240,24 @@ Matrix<double> create_matrix(const std::vector<Id>& entity_ids, const std::vecto
 
 int main(int arg_num, char** args) {
 
-  process_args(arg_num,args);
+  ProgArgs prog_args;
+  process_args(arg_num,args,prog_args);
 
-  std::vector<Id> entity_ids;
-  std::vector<Preferences> entity_preferences;
-  parse_csv_data(entity_ids, entity_preferences);
-  print_data(entity_ids,entity_preferences,std::cout);
+  auto participants = parse_csv_data();
 
-  Matrix<double> cost_matrix = std::move(create_matrix(entity_ids,entity_preferences,entity_preferences.front().size()));
+  uint group_count = participants.front().preferences.size();
+
+  Matrix<double> cost_matrix = std::move(create_matrix(participants, group_count));
   Matrix<double> solution = cost_matrix;
   Munkres munkres;
   munkres.solve(solution);
 
-  print_matrix<double>(solution,std::cerr);
+  auto assignments = create_assignments(participants,group_count,solution);
+  if(assignments.size() != participants.size()){
+    ERROR(-1,"Assignment went wrong. The reason is probably bad programming.");
+  }
+
+  print_assignments_json(participants,assignments);
 
   return 0;
 }
